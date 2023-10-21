304f085cd15e9359ef0e90b1a0cc18c2d5ba0847
TheThomasD <35131193+TheThomasD@users.noreply.github.com>
2023-10-21 16:40:59 +0200

    brought it to life!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Hello World!");
+  digitalWrite(8,1);
+  delay(1000);
+  digitalWrite(8,0);
+  delay(1000);
 }
