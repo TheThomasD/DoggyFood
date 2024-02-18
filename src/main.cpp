2f13bf952955cd80b846256e63347ab64b24e00a
TheThomasD <35131193+TheThomasD@users.noreply.github.com>
2024-02-18 09:43:08 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
-
-#define ENABLE_GxEPD2_GFX 1
-
+#include <WiFi.h>
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 
@@ -17,7 +15,8 @@ void helloWorld()
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
   display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
   // center the bounding box by transposition of the origin:
   uint16_t x = ((display.width() - tbw) / 2) - tbx;
@@ -27,20 +26,19 @@ void helloWorld()
   do
   {
     display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y-tbh);
+    display.setCursor(x, y - tbh);
     display.print(HelloWorld);
     display.setTextColor(display.epd2.hasColor ? GxEPD_RED : GxEPD_BLACK);
     display.getTextBounds(HelloWeACtStudio, 0, 0, &tbx, &tby, &tbw, &tbh);
     x = ((display.width() - tbw) / 2) - tbx;
-    display.setCursor(x, y+tbh);
+    display.setCursor(x, y + tbh);
     display.print(HelloWeACtStudio);
-  }
-  while (display.nextPage());
+  } while (display.nextPage());
 }
 
 void helloFullScreenPartialMode()
 {
-  //Serial.println("helloFullScreenPartialMode");
+  // Serial.println("helloFullScreenPartialMode");
   const char fullscreen[] = "full screen update";
   const char fpm[] = "fast partial mode";
   const char spm[] = "slow partial mode";
@@ -48,9 +46,10 @@ void helloFullScreenPartialMode()
   display.setPartialWindow(0, 0, display.width(), display.height());
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
-  if (display.epd2.WIDTH < 104) display.setFont(0);
+  if (display.epd2.WIDTH < 104)
+    display.setFont(0);
   display.setTextColor(GxEPD_BLACK);
-  const char* updatemode;
+  const char *updatemode;
   if (display.epd2.hasFastPartialUpdate)
   {
     updatemode = fpm;
@@ -64,7 +63,8 @@ void helloFullScreenPartialMode()
     updatemode = npm;
   }
   // do this outside of the loop
-  int16_t tbx, tby; uint16_t tbw, tbh;
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
   // center update text
   display.getTextBounds(fullscreen, 0, 0, &tbx, &tby, &tbw, &tbh);
   uint16_t utx = ((display.width() - tbw) / 2) - tbx;
@@ -87,8 +87,7 @@ void helloFullScreenPartialMode()
     display.print(fullscreen);
     display.setCursor(umx, umy);
     display.print(updatemode);
-  }
-  while (display.nextPage());
+  } while (display.nextPage());
   Serial.println("helloFullScreenPartialMode done");
 }
 
@@ -102,11 +101,13 @@ void showPartialUpdate()
   uint16_t box_w = 70;
   uint16_t box_h = 20;
   uint16_t cursor_y = box_y + box_h - 6;
-  if (display.epd2.WIDTH < 104) cursor_y = box_y + 6;
+  if (display.epd2.WIDTH < 104)
+    cursor_y = box_y + 6;
   float value = 13.95;
   uint16_t incr = display.epd2.hasFastPartialUpdate ? 1 : 3;
   display.setFont(&FreeMonoBold9pt7b);
-  if (display.epd2.WIDTH < 104) display.setFont(0);
+  if (display.epd2.WIDTH < 104)
+    display.setFont(0);
   display.setTextColor(GxEPD_BLACK);
   // show where the update box is
   for (uint16_t r = 0; r < 4; r++)
@@ -117,20 +118,18 @@ void showPartialUpdate()
     do
     {
       display.fillRect(box_x, box_y, box_w, box_h, GxEPD_BLACK);
-      //display.fillScreen(GxEPD_BLACK);
-    }
-    while (display.nextPage());
+      // display.fillScreen(GxEPD_BLACK);
+    } while (display.nextPage());
     delay(2000);
     display.firstPage();
     do
     {
       display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
-    }
-    while (display.nextPage());
+    } while (display.nextPage());
     delay(1000);
   }
-  //return;
-  // show updates in the update box
+  // return;
+  //  show updates in the update box
   for (uint16_t r = 0; r < 4; r++)
   {
     display.setRotation(r);
@@ -143,8 +142,7 @@ void showPartialUpdate()
         display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
         display.setCursor(box_x, cursor_y);
         display.print(value * i, 2);
-      }
-      while (display.nextPage());
+      } while (display.nextPage());
       delay(500);
     }
     delay(1000);
@@ -152,30 +150,34 @@ void showPartialUpdate()
     do
     {
       display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
-    }
-    while (display.nextPage());
+    } while (display.nextPage());
     delay(1000);
   }
 }
 
 void setup()
 {
+  WiFi.mode(WIFI_MODE_NULL);
+  btStop();
   Serial.begin(115200);
   delay(2000);
   Serial.println("Initialized!");
   pinMode(8, OUTPUT);
 
-  display.init(115200, true, 50, true);
+  display.init(115200, true, 50, false);
 
+  Serial.println("Hello World!");
   helloWorld();
+  Serial.println("FullScreenPartialMode");
   helloFullScreenPartialMode();
   delay(1000);
   if (display.epd2.hasFastPartialUpdate)
   {
+    Serial.println("PartialUpdate");
     showPartialUpdate();
     delay(1000);
   }
-  //display.hibernate();
+  display.hibernate();
 }
 
 void loop()
